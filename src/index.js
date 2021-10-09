e05bc88c5514ba26866f3ca8736ed6d3f4a00070
Zohar Nyego <zoharngo@gmail.com>
2021-10-09 21:56:40 +0300

    remove basename

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename='/cycode-challange'>
+    <Router>
       <App />
     </Router>
   </React.StrictMode>,
